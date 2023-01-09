54eb4f37db6e06d33b282685f451a81f7261f295
RemiChavance <remi.chavance@orange.fr>
2023-01-09 15:24:31 +0100

    test

---
+++ src/test/java/org/monopoly/DeTest.java
@@ -0,0 +1,14 @@
+package org.monopoly;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DeTest {
+
+    @Test
+    public void testBetween1and6() {
+        De de = new De();
+        int valeurlancer = de.getValeur();
+        assertThat(valeurlancer).isBetween(1, 6);
+    }
+}
